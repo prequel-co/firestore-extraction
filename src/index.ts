d1c89d7e7ec6d2151e7683313d72f33321ac7686
Conor <conormccarter@gmail.com>
2021-05-31 14:50:18 -0400

    remove old comments

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-//const escapeHtml = require('escape-html');
-
-/**
- * HTTP Cloud Function.
- *
- * @param {Object} req Cloud Function request context.
- *                     More info: https://expressjs.com/en/api.html#req
- * @param {Object} res Cloud Function response context.
- *                     More info: https://expressjs.com/en/api.html#res
- */
-
 import { Firestore } from '@google-cloud/firestore'
 import { Storage } from '@google-cloud/storage'
 import { PubSub, Attributes } from '@google-cloud/pubsub'
