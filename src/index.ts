ac3ad00b336a82e880b73e6f2a2e4ed198debf36
Conor <conormccarter@gmail.com>
2021-06-07 23:44:26 -0400

    add create and update time

--- src/index.ts
+++ src/index.ts
@@ -160,7 +160,7 @@ const extractByBatch = async (e: any, context: any, callback: any) => {
       } else {
         console.log(doc)
       }
-      writestream.write(JSON.stringify({key: doc.id, value: doc.data()}, null, 2))
+      writestream.write(JSON.stringify({id: doc.id, data: doc.data(), createTime: doc.createTime, updateTime: doc.updateTime}, null, 2))
     })
 
     isFirstBatch = false
